8aa19866dc78705c7f11bf673cb96ff5e3b18a2d
page14 <70405414+page14@users.noreply.github.com>
2022-07-14 22:23:32 -0400

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,42 @@
+var milk
+
+
+function preload()
+{
+	//load images here
+ milk = loadImage('../images/Milk.png');
+
+}
+
+    class Food { 
+        constructor(height, width) {
+    
+    }
+
+     
+    display(){
+      var x=80, y=100;
+    
+      imageMode(CENTER);
+      image(this.image,720,220,70,70);
+    
+      if(this.foodStock!=0){
+      for(var i=0;i<this.foodStock;i++){
+          if(i%10==0){
+              x=80;
+              y+y+50;
+          }
+          image(this.image,x,y,50,50);
+          x=x+30;
+          }
+        }
+      }
+      
+}
+
+
+function draw(){
+    
+}
+
+ 
